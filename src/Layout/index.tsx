a139e0cce59768af3178834b4e18145cae1377c9
lbj mac air m3 <1325349854@qq.com>
2024-12-03 18:16:59 +0800

    默认将main部分占满剩余空间

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -33,7 +33,7 @@ export default function Layout(props: {
 }) {
   const { children } = props;
   return (
-    <AppTheme {...props} themeComponents={xThemeComponents}>
+    <AppTheme {...props} themeComponents={xThemeComponents as any}>
       <CssBaseline enableColorScheme />
       <Box sx={{ display: "flex" }}>
         <SideMenu />
@@ -47,19 +47,32 @@ export default function Layout(props: {
               ? `rgba(${theme.vars.palette.background.defaultChannel} / 1)`
               : alpha(theme.palette.background.default, 1),
             overflow: "auto",
+            minHeight: "100vh",
           })}
         >
           <Stack
             spacing={2}
             sx={{
-              alignItems: "center",
+              // alignItems: "center",
               mx: 3,
               pb: 5,
               mt: { xs: 8, md: 0 },
+              flex: 1,
+              flexDirection: "column",
+              height: "100%",
             }}
           >
             <Header />
-            {children}
+            <Box
+              sx={{
+                flexGrow: 1, // 添加这一属性，让Box组件在Stack中占满剩余空间
+                display: "flex",
+                flexDirection: "column", // 明确Box内部也是垂直方向布局（根据实际需求确定）
+                justifyContent: "space-between", // 这里可以根据需要保留或者去除，如果想让Box内部子元素有间隔分布效果可保留
+              }}
+            >
+              {children}
+            </Box>
           </Stack>
         </Box>
       </Box>
--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -4,9 +4,7 @@ export default function ChatLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section style={{width: '100%'}}>
-      <nav>123</nav>
-
+    <section style={{width: '100%', height: '100%'}}>
       {children}
     </section>
   );
