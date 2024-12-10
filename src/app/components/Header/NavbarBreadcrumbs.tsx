02b2484281af86913467f06c7576c5afbbdde1f2
lbj mac air m3 <1325349854@qq.com>
2024-12-10 12:41:28 +0800

    面包屑可用

--- src/app/components/Header/NavbarBreadcrumbs.tsx
+++ src/app/components/Header/NavbarBreadcrumbs.tsx
@@ -3,28 +3,35 @@ import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import Breadcrumbs, { breadcrumbsClasses } from '@mui/material/Breadcrumbs';
 import NavigateNextRoundedIcon from '@mui/icons-material/NavigateNextRounded';
+import { useRouter, useSearchParams, usePathname } from 'next/navigation'
 
 const StyledBreadcrumbs = styled(Breadcrumbs)(({ theme }) => ({
   margin: theme.spacing(1, 0),
-  [`& .${breadcrumbsClasses.separator}`]: {
+  [`&.${breadcrumbsClasses.separator}`]: {
     color: (theme.vars || theme).palette.action.disabled,
     margin: 1,
   },
-  [`& .${breadcrumbsClasses.ol}`]: {
+  [`&.${breadcrumbsClasses.ol}`]: {
     alignItems: 'center',
   },
 }));
 
 export default function NavbarBreadcrumbs() {
+  const pathname = usePathname();
+  console.log('pathname--', pathname);
+  const pathSegments = pathname?.split('/').filter(segment => segment !== '');
+  console.log('pathSegments--', pathSegments);
+
   return (
     <StyledBreadcrumbs
       aria-label="breadcrumb"
       separator={<NavigateNextRoundedIcon fontSize="small" />}
     >
-      <Typography variant="body1">Dashboard</Typography>
-      <Typography variant="body1" sx={{ color: 'text.primary', fontWeight: 600 }}>
-        Home
-      </Typography>
+      {pathSegments.map((segment, index) => (
+        <Typography key={index} variant="body1" sx={{ color: index === pathSegments.length - 1? 'text.primary' : 'inherit', fontWeight: index === pathSegments.length - 1? 600 : 400 }}>
+          {segment}
+        </Typography>
+      ))}
     </StyledBreadcrumbs>
   );
 }
