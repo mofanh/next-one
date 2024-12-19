9cb92910feb97a47a1026d96dede9c918dbf582a
li.bojing <1325349854@qq.com>
2024-12-19 23:20:38 +0800

    global.css

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,12 @@
 export default async function Home() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/albums");
-  if (!res.ok) {
-    throw new Error(`HTTP error! status: ${res.status}`);
-  }
-  const albums = await res.json();
+  // const res = await fetch("https://jsonplaceholder.typicode.com/albums");
+  // if (!res.ok) {
+  //   throw new Error(`HTTP error! status: ${res.status}`);
+  // }
+  // const albums = await res.json();
   return (
     <main>
-      {albums.map((album: { id: number; title: string }) => {
-        return (
-          <div
-          key={album.id}
-          className="shadow-md rounded-lg p-4 transition">
-            <h3 className="text-lg font-bold mb-2">{album.title}</h3>
-            <p className="text-gray-600">id: {album.id}</p>
-          </div>
-        );
-      })}
+      <h1 className="heading">Home</h1>
     </main>
   );
 }
