cbb3d6420a59e70739735251cfaaf663acbbcd44
li.bojing <1325349854@qq.com>
2024-12-21 00:53:21 +0800

    新增主页

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,26 @@
-import SearchForm from "../components/SearchForm";
+import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({
   searchParams,
-}: Promise<{ query: string }>) {
-  const query = (await searchParams).query;
+}: {
+  searchParams: Promise<{ query: string }>;
+}) {
+  const { query } = await searchParams;
   return (
     <main>
       <section className="pink_container">
         <h1 className="heading">Next one</h1>
         <p className="sub-heading">submit idea</p>
         <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Results for "${query}"` : "All startups"}
+        </p>
+        <ul>
+          <StartupCard />
+        </ul>
       </section>
     </main>
   );
